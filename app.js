bd0016824b0e1da42292282d0c2cd211908b76b2
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-10-03 16:59:47 +0900

    add desktop view

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function animateSlides() {
     slideT1.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideT1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideT1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideT1.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
     //create scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -122,12 +122,6 @@ barba.init({
       beforeEnter() {
         logo.href = "../index.html";
         detailAnimation();
-        gsap.fromTo(
-          ".nav-header",
-          1,
-          { y: "100%" },
-          { y: "0%", ease: "power2.inOut" }
-        );
       },
       beforeLeave() {
         controller.destroy();
@@ -160,9 +154,17 @@ barba.init({
           ".swipe",
           1,
           { x: "0%" },
-          { x: "100%", stagger: 0.25, onComplete: done }
+
+          { x: "100%", stagger: 0.2, onComplete: done }
         );
         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
       },
     },
   ],

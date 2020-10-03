fdd384af48e4fd796b296e911d30c7506b481129
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-10-03 13:54:26 +0900

    add cursor

--- app.js
+++ app.js
@@ -56,10 +56,33 @@ function animateSlides() {
   });
 }
 
-// function cursor(e) {
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
 
-// }
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
 
-// window.addEventListener("mousemove", cursor);
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
 
-// animateSlides();
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
+
+animateSlides();
